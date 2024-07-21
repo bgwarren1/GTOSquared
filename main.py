825dad211b3217afc7452f10c1e9289231b0e334
Blair Warren <bwarren11303@gmail.com>
2024-07-21 00:31:22 -0500

    added main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from PIL import Image, ImageTk
+import tkinter as tk
+from board import Board
+
+
+def main():
+    
+    root = tk.Tk() # create root window
+    root.title("Poker Solver Board")
+
